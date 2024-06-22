8dea2ed8667dad8ddbc16b3823e6f5b50baa09bd
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-06-22 20:34:03 +0400

    Update Main.java
    Изменил решение вынес в отдельные методы необходимые задачи

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,6 +11,7 @@ public class Main {
     static AtomicInteger counter4 = new AtomicInteger(0);
     static AtomicInteger counter5 = new AtomicInteger(0);
 
+
     public static void main(String[] args) throws InterruptedException {
         //заполняем массив рандомно созданными строками длиной 3 - 5 букв из литеров "abc"
         Random random = new Random();
@@ -17,102 +20,99 @@ public class Main {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
-        //создается поток с лямбда выражением которое
-        // находит в массиве строк полиндромные слова
-        Thread thread1 = new Thread(() -> {
-            for (String str : texts) {
-                //проверяем что бы длина строки была 3 буквы , передаем строку в StringBuilder
-                // с помощью reverse переворачиваем строку
-                if (str.equals(new StringBuilder(str).reverse().toString())) {
-                    //если условие выполнено ,то соответственно увеличиваем
-                    // счетчик соответствующий длине слова на 1
-                    switch (str.length()) {
-                        case 3:
-                            counter3.incrementAndGet();
-                            break;
-                        case 4:
-                            counter4.incrementAndGet();
-                            break;
-                        case 5:
-                            counter5.incrementAndGet();
-                            break;
-                        default:
-                    }
-                }
-            }
-        });
-        thread1.start();//стартуем поток
-        thread1.join();//текущий поток ожидает завершения выполнения потока "thread1"
-        System.out.println("Красивых слов с длинной 3  - " + counter3);
-        System.out.println("Красивых слов с длинной 4  - " + counter4);
-        System.out.println("Красивых слов с длинной 5  - " + counter5);
+        //создается список потоков
+        List<Thread> threads = new ArrayList<>();
 
 
-        //создается поток с лямбда выражением которое определяет
-        // состоит ли слово из одинаковых букв
-        Thread thread2 = new Thread(() -> {
-            for (String text : texts) {
-                //проверяем длину строки должна быть 4 буквы , text.charAt(0) - первый символ строки
-                // метод allMatch проверяет соответствуют все символы условию заданному лямбда
-                // выражением c -> c == text.charAt(0) где "с" текущий символ
-                if (text.chars().allMatch(c -> c == text.charAt(0))) {
-                    //если условие выполнено ,то соответственно увеличиваем
-                    // счетчик соответствующий длине слова на 1
-                    switch (text.length()) {
-                        case 3:
-                            counter3.incrementAndGet();
-                            break;
-                        case 4:
-                            counter4.incrementAndGet();
-                            break;
-                        case 5:
-                            counter5.incrementAndGet();
-                            break;
-                        default:
-                    }
+        for (String str : texts) {
+            Thread thread1 = new Thread(() -> countString(str));
+            Thread thread2 = new Thread(() -> literEquals(str));
+            Thread thread3 = new Thread(() -> literABC(str));
+            threads.add(thread1);
+            threads.add(thread2);
+            threads.add(thread3);
+            thread1.start();
+            thread2.start();
+            thread3.start();
+        }
 
-                }
-            }
-        });
-        thread2.start();//стартуем поток
-        thread2.join();//текущий поток ожидает завершения выполнения потока "thread2"
-        System.out.println("\nКрасивых слов с длинной 3  - " + counter3);
-        System.out.println("Красивых слов с длинной 4  - " + counter4);
-        System.out.println("Красивых слов с длинной 5  - " + counter5);
+        for (Thread thread : threads) {
+            thread.join();
+        }
 
-
-        //создается поток с лямбда выражением которое определяет
-        // находятся ли буквы в слове по возрастанию
-        Thread thread3 = new Thread(() -> {
-            for (String txt : texts) {
-                //передаем в метод matches регулярное выражение на основании на основании
-                // которого проверяется удовлетворяет слово условию или нет
-                // также проверяем что длина слова равна 5
-                if (txt.matches("a*b*c*")) {
-                    //если условие выполнено ,то соответственно увеличиваем
-                    // счетчик соответствующий длине слова на 1
-                    switch (txt.length()) {
-                        case 3:
-                            counter3.incrementAndGet();
-                            break;
-                        case 4:
-                            counter4.incrementAndGet();
-                            break;
-                        case 5:
-                            counter5.incrementAndGet();
-                            break;
-                        default:
-                    }
-
-                }
-            }
-        });
-        thread3.start();//стартуем поток
-        thread3.join();//текущий поток ожидает завершения выполнения потока "thread3"
-        System.out.println("\nКрасивых слов с длинной 3  - " + counter3);
-        System.out.println("Красивых слов с длинной 4  - " + counter4);
-        System.out.println("Красивых слов с длинной 5  - " + counter5);
+        System.out.println("Красивых слов длиной 3 буквы : " + counter3
+                + "\nКрасивых слов длиной 4 буквы : " + counter4
+                + "\nКрасивых слов длиной 5 букв : " + counter5);
     }
+
+
+    //метод для проверки слов на полиндромность
+    public static void countString(String text) {
+        //передаем строку в StringBuilder
+        // с помощью reverse переворачиваем строку
+        if (text.equals(new StringBuilder(text).reverse().toString())) {
+            //если условие выполнено ,то соответственно увеличиваем
+            // счетчик соответствующий длине слова на 1
+            switch (text.length()) {
+                case 3:
+                    counter3.incrementAndGet();
+                    break;
+                case 4:
+                    counter4.incrementAndGet();
+                    break;
+                case 5:
+                    counter5.incrementAndGet();
+                    break;
+                default:
+            }
+        }
+    }
+
+
+    // метод для проверки слов на то что в слове все буквы одинаковые
+    public static void literEquals(String text) {
+        if (text.chars().allMatch(c -> c == text.charAt(0))) {
+            //если условие выполнено ,то соответственно увеличиваем
+            // счетчик соответствующий длине слова на 1
+            switch (text.length()) {
+                case 3:
+                    counter3.incrementAndGet();
+                    break;
+                case 4:
+                    counter4.incrementAndGet();
+                    break;
+                case 5:
+                    counter5.incrementAndGet();
+                    break;
+                default:
+            }
+        }
+    }
+
+
+    //метод  определяющий находятся ли буквы в слове по возрастанию
+    public static void literABC(String text) {
+        //передаем в метод matches регулярное выражение на основании на основании
+        // которого проверяется удовлетворяет слово условию или нет
+        // также проверяем что длина слова равна 5
+        if (text.matches("a*b*c*")) {
+            //если условие выполнено ,то соответственно увеличиваем
+            // счетчик соответствующий длине слова на 1
+            switch (text.length()) {
+                case 3:
+                    counter3.incrementAndGet();
+                    break;
+                case 4:
+                    counter4.incrementAndGet();
+                    break;
+                case 5:
+                    counter5.incrementAndGet();
+                    break;
+                default:
+            }
+        }
+    }
+
 
     //метод для рандомного создания строки из заданных букв и заданной длины
     public static String generateText(String letters, int length) {
